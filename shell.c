82e682933b00ea123ef36b7767b4893c811a1b6e
guvi007 <gaurav17288@iiitd.ac.in>
2019-03-03 13:19:19 +0530

    added commands

--- shell.c
+++ shell.c
@@ -5,10 +5,24 @@
 #include<sys/wait.h>
 #include<limits.h>
 #include<signal.h>
-#include <sys/types.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+char *cmdhistory[1000];//stores the commands used
+int ncmd=0;//keeps a count of the number of commands executed
 
 /*
-	Splits the array with Type = {">", "<", " "}(Redirect from).
+pipe
+grep not working
+uniq not working
+sed not checked
+sort not working
+backspace+Tab
+1>>out.txt not append
+*/
+
+/*
+	Splits the array with Type = {">", "<", " "}.
 	Returns the final size of the splited array
 */
 int splitByType(char *line, char *array[], char *type)
@@ -24,8 +38,10 @@ int splitByType(char *line, char *array[], char *type)
 	return i;
 }
 
-int getPath(char* string, char actualPath[1000])
+char* getPath(char* string)
 {
+
+	char *actualPath;
 	char *path[1000];
 	char* currentPath = getcwd(NULL,0);
 	int elements = splitByType(string,path,"/");
@@ -39,13 +55,11 @@ int getPath(char* string, char actualPath[1000])
 			strcat(nowPath,path[h]);
 			strcat(nowPath,"/");
 		}
-		printf("%s\n",nowPath);
 		int checkDirectory=chdir(nowPath);
-		printf("%d\n",checkDirectory);
 		if(checkDirectory<0)
 		{
 			printf("Invalid Path");
-			return -1;
+			return "";
 		}
 		actualPath = getcwd(NULL,0);
 		strcat(actualPath,"/");
@@ -58,13 +72,171 @@ int getPath(char* string, char actualPath[1000])
 	}
 
 	chdir(currentPath);
-	printf("%s\n", actualPath);
-	return 0;
+	strcat(actualPath,"\0");
+	return actualPath;
+}
+
+void checkfile(int fileno,char *path)
+{
+	if( fileno == -1)
+	{
+			FILE * fPtr;
+			fPtr = fopen(path,"w");
+			if(fPtr == NULL)
+    	{
+        /* File not created hence exit */
+        printf("Unable to create file.\n");
+    	}
+			fclose(fPtr);
+	}
+}
+
+int inputfd(char *input)
+{
+	if(strlen(input) > 0)
+	{
+
+		if(input[0] == '&')
+		{
+			if(strlen(input)==2 && (input[1] == '1' || input[1] == '2' || input[1] == '0'))
+			{
+				if(input[1] == '1') {
+					close(0);
+					dup(1);
+				}
+				else if(input[1] == '2') {
+					close(0);
+					dup(2);
+				}
+				return 1;
+			}
+			else
+			{
+				printf("%s\n", "Bad File Descriptor.");
+				return 0;
+			}
+		}
+
+		char *inputPath;
+		inputPath = getPath(input);
+		if(strcmp(inputPath,"\0") == 0){
+			printf("%s\n","Path is Invalid " );
+			return 0;
+		}
+		else{
+			int file = open(inputPath, O_RDONLY);
+			if(file != -1) {
+				close(0);
+				dup(file);
+			}
+			else {
+				printf("%s\n", "File Doesn't Exist");
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+int outputfd(char *output, int isappend)
+{
+	if(strlen(output) > 0)
+	{
+		if(output[0] == '&')
+		{
+			if(strlen(output)==2 && (output[1] == '1' || output[1] == '2' || output[1] == '0'))
+			{
+				if(output[1] == '0') {
+					close(1);
+					dup(0);
+				}
+				else if(output[1] == '2') {
+					close(1);
+					dup(2);
+				}
+				return 1;
+			}
+			else
+			{
+				printf("%s\n", "Bad File Descriptor.");
+				return 0;
+			}
+		}
+
+		char* outputPath;
+		printf("%s\n", output);
+		outputPath = getPath(output);
+		if(strcmp(outputPath,"\0") == 0){
+			printf("%s\n","Path is Invalid " );
+			return 0;
+		}
+		else{
+			int file;
+			if(isappend == 1) {
+				file = open(outputPath,O_WRONLY | O_APPEND);
+				checkfile(file, outputPath);
+				file = open(outputPath,O_WRONLY | O_APPEND);
+			}
+			else {
+				remove(outputPath);
+				file = open(outputPath,O_WRONLY);
+				checkfile(file, outputPath);
+				file = open(outputPath,O_WRONLY);
+			}
+
+			close(1);
+			dup(file);
+		}
+	}
+	return 1;
+}
+
+int errorfd(char *error)
+{
+	if(strlen(error) > 0)
+	{
+
+		if(error[0] == '&')
+		{
+			if(strlen(error)==2 && (error[1] == '1' || error[1] == '2' || error[1] == '0'))
+			{
+				if(error[1] == '0') {
+					close(2);
+					dup(0);
+				}
+				else if(error[1] == '1') {
+					close(2);
+					dup(1);
+				}
+				return 1;
+			}
+			else
+			{
+				printf("%s\n", "Bad File Descriptor.");
+				return 0;
+			}
+		}
+
+		char* errorPath;
+		errorPath = getPath(error);
+		if(strcmp(errorPath,"\0") == 0){
+			return 0;
+		}
+		else{
+			int file = open(errorPath,O_WRONLY);
+			checkfile(file, errorPath);
+			file = open(errorPath,O_WRONLY);
+			close(2);
+			dup(file);
+		}
+	}
+	return 1;
 }
 
 void execute(char *executable,char *input,char *output,char *error,char *attribute,
-						int numArguments,char	arguments[100][1000], int isappend)
+						int numArguments,char	arguments[100][1000], int isappend,int inputIndex,int outputIndex,int errorIndex)
 {
+
 		pid_t pid;
 		int status;
 
@@ -78,29 +250,21 @@ void execute(char *executable,char *input,char *output,char *error,char *attribu
 				return;
 		}
 
-
-		if(strlen(input) > 0)
+		if(strcmp(executable, "clear") == 0)
 		{
-			char inputPath[1000];
-			memset(inputPath,'\0', 1000*sizeof(char));
-			int val = getPath(input,inputPath);
-			printf("%d\n", val);
+			printf("\e[1;1H\e[2J");
 		}
 
-		if(strlen(output) > 0)
-		{
-			char outputPath[1000];
-			memset(outputPath,'\0', 1000*sizeof(char));
-			int val = getPath(output,outputPath);
-			printf("%d\n", val);
-			printf("%s\n", outputPath);
-		}
-
-		if(strlen(error) > 0)
-		{
-			char errorPath[1000];
-			memset(errorPath,'\0', 1000*sizeof(char));
-			int val = getPath(error,errorPath);
+		if(strcmp(executable, "history") == 0) {
+			if(ncmd == 0) {
+				printf("%s\n", "No Commands Found");
+			}
+			else {
+				for(int i=0; i<ncmd; ++i) {
+					printf("%d %s\n",i+1,cmdhistory[i]);
+				}
+			}
+			return;
 		}
 
 		pid = fork();
@@ -111,6 +275,59 @@ void execute(char *executable,char *input,char *output,char *error,char *attribu
 		}
 		else if(pid == 0)
 		{
+
+			if(inputIndex < outputIndex && inputIndex < errorIndex){
+				int val = inputfd(input);
+				if(val == 0) return;
+				if(outputIndex<errorIndex){
+					val = outputfd(output, isappend);
+					if(val == 0) return;
+					val = errorfd(error);
+					if(val == 0) return;
+				}
+				else{
+					val = errorfd(error);
+					if(val == 0) return;
+					val = outputfd(output, isappend);
+					if(val == 0) return;
+				}
+			}
+			else if(outputIndex < inputIndex  && outputIndex < errorIndex){
+				int val = outputfd(output,isappend);
+				if(val == 0) return;
+				if(inputIndex<errorIndex){
+					val = inputfd(input);
+					if(val == 0) return;
+					val = errorfd(error);
+					if(val == 0) return;
+				}
+				else{
+					val = errorfd(error);
+					if(val == 0) return;
+					val = inputfd(input);
+					if(val == 0) return;
+				}
+			}
+			else
+			{
+				int val = errorfd(error);
+				if(val == 0) return;
+				if(inputIndex < outputIndex){
+					val = inputfd(input);
+					if(val == 0) return;
+					val = outputfd(output, isappend);
+					if(val == 0) return;
+				}
+				else{
+					val=outputfd(output,isappend);
+					if(val == 0) return;
+				  val = inputfd(input);
+					if(val == 0) return;
+				}
+			}
+
+			if(strcmp(executable,"") == 0) return;
+
 			char *args[1000];
 			char command[1000] = "/bin/";
 			strcat(command, executable);
@@ -142,20 +359,6 @@ void execute(char *executable,char *input,char *output,char *error,char *attribu
 		else {
 			while(wait(&status) != pid);
 		}
-}
-
-void proceed(char *executable,char *input,char *output,char *error,char *attribute,
-						int numArguments,char	arguments[100][1000], int isappend)
-{
-	printf("output = %s\n", output);
-	printf("input = %s\n", input);
-	printf("error = %s\n", error);
-	printf("#attributes = %s\n", attribute);
-	printf("executable = %s\n", executable);
-	for(int i=0; i<numArguments; ++i) {
-		printf("arguments = %s\n", arguments[i]);
-	}
-	execute(executable,input,output,error,attribute,numArguments,arguments, isappend);
 }
 
 /*
@@ -255,26 +458,6 @@ void determineExec(char command[])
 					command[y] = ' ';
 				}
 		}
-		/*
-		else if(command[g]=='>' && command[g+1] == '>')
-		{
-			if(g-2 < 0 || command[g-2] == ' ')
-			{
-				memset(input,'\0', 1000*sizeof(char));
-				int fstart=removeSpacing(command,g,lengthString);
-				int fend=findAttribute(fstart+1,command,input);
-				inputind = g;
-				for(int y=g-1; y<fend; ++y)
-				{
-					command[y] = ' ';
-				}
-			}
-			else{
-				printf("%s\n", "Invalid Command wrong redirection");
-				return;
-			}
-		}
-		*/
 		else if(command[g] == '<' && (command[g-1] == '2' || command[g-1] == '1' || command[g-1] == '0'))
 		{
 			printf("%s\n", "Invalid Command wrong redirection");
@@ -292,6 +475,7 @@ void determineExec(char command[])
 	i = findAttribute(i+1, command, executable);
 
 	if(strcmp("exit", executable) == 0) {
+		printf("%s","\e[1;1H\e[2J");
 		exit(0);
 	}
 
@@ -318,6 +502,7 @@ void determineExec(char command[])
 			{
 				isappend = 1;
 				i+=1;
+				continue;
 			}
 			if(i < outputind) {
 				char temp[1000] = "";
@@ -359,16 +544,15 @@ void determineExec(char command[])
 		}
 	}
 
-	proceed(executable, input, output, error, attribute, numArguments, arguments, isappend);
-
-	// printf("output = %s\n", output);
-	// printf("input = %s\n", input);
-  // printf("error = %s\n", error);
-	// printf("#attributes = %s\n", attribute);
-	// printf("executable = %s\n", executable);
-	// for(int i=0; i<numArguments; ++i) {
-	// 	printf("arguments = %s\n", arguments[i]);
-	// }
+	printf("output = %s\n", output);
+	printf("input = %s\n", input);
+	printf("error = %s\n", error);
+	printf("#attributes = %s\n", attribute);
+	printf("executable = %s\n", executable);
+	for(int i=0; i<numArguments; ++i) {
+		printf("arguments = %s\n", arguments[i]);
+	}
+	execute(executable,input,output,error,attribute,numArguments,arguments, isappend, inputind, outputind, errorind);
 }
 
 /*
@@ -398,40 +582,13 @@ void Input()
 {
 	// taking input
 	printf("\e[36;1m~%s>$ \e[0m", getcwd(NULL,0));
-	// char line[1000] = "";
-  // char c = getchar();
-	// int i = 0;
-	// line[i] = c;
-  // while (c != '\n' && c != EOF) {
-	// 		i += 1;
-	// 		c = getchar();
-	// 		if(c == '\t') {
-	// 			exit(0);
-	// 			printf("%s\n", "hi");
-	// 			line[i] = ' ';
-	// 			line[i+1] = ' ';
-	// 			line[i+2] = ' ';
-	// 			line[i+3] = ' ';
-	// 			i += 3;
-	// 		}
-	// 		else if(c == '\b'){
-	// 			printf("%s\n", "backspace");
-	// 			--i;
-	// 			if(i<-1) i = 0;
-	// 		}
-	// 		else {
-	// 			printf("%s\n", "normal");
-	// 			line[i] = c;
-	// 		}
-	// 		printf("%s\n", line);
-	// }
-	// line[i] = '\0';
 	char* command=malloc(sizeof(char)*1000);
 	size_t n = 1000;
 	getline(&command, &n, stdin);
 	command[strlen(command)-1]='\0';
 	if(strcmp(command,"\n")==0)
 		return;
+	cmdhistory[ncmd++] = command;
 	splitByPipe(command);
 }
 
@@ -447,9 +604,10 @@ void sig(int sigi)
 	Input();
 }
 
-void main() {
-	setvbuf(stdin, NULL, _IONBF, 0);
+void main()
+{
 	signal(SIGINT, sig);
+	printf("%s","\e[1;1H\e[2J");
 	while(1) {
 		Input();
 	}
